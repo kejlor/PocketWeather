8ea0e79daf4e37d575388c59b9bc20b4a818f2be
kejlor <woj450@gmail.com>
2023-04-08 03:52:25 +0200

    initial commit

---
+++ PocketWeather/PocketWeather/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  PocketWeather
+//
+//  Created by Bartosz Wojtkowiak on 08/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let stackView = UIStackView()
+    let label = UILabel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        style()
+        layout()
+    }
+}
+
+extension ViewController {
+    func style() {
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Welcome"
+        label.font = UIFont.preferredFont(forTextStyle: .title1)
+    }
+    
+    func layout() {
+        stackView.addArrangedSubview(label)
+        
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        ])
+    }
+}
+
