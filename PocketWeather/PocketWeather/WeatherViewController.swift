45cb29b49261b775c5aab3db235c462b98c48c33
kejlor <woj450@gmail.com>
2023-04-17 18:14:22 +0200

    add: scroll screen to a text field during typing

--- PocketWeather/PocketWeather/WeatherViewController.swift
+++ PocketWeather/PocketWeather/WeatherViewController.swift
@@ -27,6 +27,12 @@ class WeatherViewController: UIViewController {
         super.viewDidLoad()
         style()
         layout()
+        setupKeyboardHiding()
+    }
+    
+    private func setupKeyboardHiding() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
 }
 
@@ -137,5 +143,30 @@ extension WeatherViewController {
         self.weatherView.descriptionLabelView.label.text = weather.weather[0].description
         self.weatherView.weatherTextLabel.text = weather.weather[0].main
         self.weatherView.checkWeatherImage()
+    }
+}
+
+// MARK: Keyboard
+extension WeatherViewController {
+    @objc func keyboardWillShow(sender: NSNotification) {
+        guard let userInfo = sender.userInfo,
+              let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue,
+              let currentTextField = UIResponder.currentFirst() as? UITextField else { return }
+
+        // check if the top of the keyboard is above the bottom of the currently focused textbox
+        let keyboardTopY = keyboardFrame.cgRectValue.origin.y
+        let convertedTextFieldFrame = view.convert(currentTextField.frame, from: currentTextField.superview)
+        let textFieldBottomY = convertedTextFieldFrame.origin.y + convertedTextFieldFrame.size.height
+
+        // if textField bottom is below keyboard bottom - bump the frame up
+        if textFieldBottomY > keyboardTopY {
+            let textBoxY = convertedTextFieldFrame.origin.y
+            let newFrameY = (textBoxY - keyboardTopY / 2) * -1
+            view.frame.origin.y = newFrameY
+        }
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        view.frame.origin.y = 0
     }
 }
